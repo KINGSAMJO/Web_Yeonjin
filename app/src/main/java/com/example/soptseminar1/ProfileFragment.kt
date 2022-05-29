131275bb78af1eafa0dfeab2d49a98a94dc466f6
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-05-29 23:25:33 +0900

    [feature] Github API 프로필 연동

--- app/src/main/java/com/example/soptseminar1/ProfileFragment.kt
+++ app/src/main/java/com/example/soptseminar1/ProfileFragment.kt
@@ -1,13 +1,16 @@
 package com.example.soptseminar1
 
 import android.os.Bundle
-import android.telecom.Call
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.example.soptseminar1.databinding.FragmentProfileBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
 
@@ -20,14 +23,43 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        userInfoNetworking()
         initTransactionEvent()
-        initImage()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun userInfoNetworking(){
+        val call: Call<ResponseGithubUserInformation> = GithubApiCreator.githubApiService.fetchGithubUserInformation()
+
+        call.enqueue(object : Callback<ResponseGithubUserInformation> {
+            override fun onResponse(
+                call: Call<ResponseGithubUserInformation>,
+                response: Response<ResponseGithubUserInformation>
+            ) {
+                if(response.isSuccessful){
+                    val data = response.body()
+                    data?.let {
+                        Glide.with(this@ProfileFragment)
+                            .load(data.avatar_url)
+                            .circleCrop()
+                            .into(binding.myImage)
+                        binding.myName.text = data.name
+                        binding.myId.text = data.userId
+                        binding.myBio.text = data.bio
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseGithubUserInformation>, t: Throwable) {
+                Log.e("NetworkTest", "error:$t")
+            }
+
+        })
     }
 
     private fun initTransactionEvent() {
@@ -64,13 +96,6 @@ class ProfileFragment : Fragment() {
             }
             transaction.commit()
         }
-    }
-
-    private fun initImage() {
-        Glide.with(this)
-            .load(R.drawable.pic)
-            .circleCrop()
-            .into(binding.myImage)
     }
 
     companion object {
