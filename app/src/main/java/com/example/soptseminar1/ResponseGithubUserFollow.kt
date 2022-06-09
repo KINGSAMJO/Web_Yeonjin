413267f87c02ba2a94116189f883b9a9d7c99bf2
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-06-09 20:10:35 +0900

    [chore/#5] 중복 파일 삭제

--- app/src/main/java/com/example/soptseminar1/ResponseGithubUserFollow.kt
+++
@@ -1,9 +0,0 @@
-package com.example.soptseminar1
-
-import com.google.gson.annotations.SerializedName
-
-data class ResponseGithubUserFollow(
-    @SerializedName("login")
-    val userId : String,
-    val avatar_url : String
-)
