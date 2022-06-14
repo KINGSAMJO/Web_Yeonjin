675fb36780b75632bde2abede5a68218af347e45
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-06-14 23:32:02 +0900

    [refactor/#5] Android KTX

--- app/src/main/java/com/example/soptseminar1/data/api/SOPTSharedPreferences.kt
+++ app/src/main/java/com/example/soptseminar1/data/api/SOPTSharedPreferences.kt
@@ -2,12 +2,13 @@ package com.example.soptseminar1.data.api
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.core.content.edit
 
 object SOPTSharedPreferences {
     private const val STORAGE_KEY = "USER_AUTH"
     private const val AUTO_LOGIN = "AUTO_LOGIN"
 
-    fun getSharedPreference(context: Context): SharedPreferences{
+    private fun getSharedPreference(context: Context): SharedPreferences {
         return context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
     }
 
@@ -16,17 +17,10 @@ object SOPTSharedPreferences {
     }
 
     fun setAutoLogin(context: Context, value: Boolean) {
-        getSharedPreference(context)
-            .edit()
-            .putBoolean(AUTO_LOGIN, value)
-            .apply()
+        getSharedPreference(context).edit { putBoolean(AUTO_LOGIN, value) }
     }
 
     fun setLogout(context: Context) {
-        getSharedPreference(context)
-            .edit()
-            .remove(AUTO_LOGIN)
-            .clear()
-            .apply()
+        getSharedPreference(context).edit { remove(AUTO_LOGIN).clear() }
     }
 }
