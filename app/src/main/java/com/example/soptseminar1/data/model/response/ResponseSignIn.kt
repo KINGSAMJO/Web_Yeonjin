d595beb96b6866e2d8b39b2125b9ea99ac3a8e60
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-08-02 15:41:59 +0900

    [refactor/#7] 리스폰스 객체 수정

--- app/src/main/java/com/example/soptseminar1/data/model/response/ResponseSignIn.kt
+++ app/src/main/java/com/example/soptseminar1/data/model/response/ResponseSignIn.kt
@@ -1,12 +1,6 @@
 package com.example.soptseminar1.data.model.response
 
 data class ResponseSignIn(
-    val status: Int,
-    val message: String,
-    val data: Data
-) {
-    data class Data(
-        val email: String,
-        val name: String
-    )
-}
+    val email: String,
+    val name: String
+)
