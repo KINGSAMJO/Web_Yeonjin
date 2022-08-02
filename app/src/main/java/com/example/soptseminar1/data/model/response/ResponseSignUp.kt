35fbe5646b5455903f8b53047df61fd2fb525216
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-08-02 15:53:02 +0900

    [refactor/#7] SignUp 코루틴 적용

--- app/src/main/java/com/example/soptseminar1/data/model/response/ResponseSignUp.kt
+++ app/src/main/java/com/example/soptseminar1/data/model/response/ResponseSignUp.kt
@@ -3,4 +3,3 @@ package com.example.soptseminar1.data.model.response
 data class ResponseSignUp(
     val id: Int
 )
-
--- app/src/main/java/com/example/soptseminar1/presentation/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/soptseminar1/presentation/signup/SignUpActivity.kt
@@ -2,18 +2,24 @@ package com.example.soptseminar1.presentation.signup
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.example.soptseminar1.presentation.signin.SignInActivity
 import com.example.soptseminar1.data.api.ServiceCreator
+import com.example.soptseminar1.data.api.SoptService
 import com.example.soptseminar1.data.model.request.RequestSignUp
 import com.example.soptseminar1.data.model.response.ResponseSignUp
 import com.example.soptseminar1.databinding.ActivitySignUpBinding
 import com.example.soptseminar1.util.BaseActivity
 import com.example.soptseminar1.util.enqueueUtil
 import com.example.soptseminar1.util.showToast
+import kotlinx.coroutines.launch
 import retrofit2.Call
 
 class SignUpActivity : BaseActivity<ActivitySignUpBinding>({
-    ActivitySignUpBinding.inflate(it) }) {
+    ActivitySignUpBinding.inflate(it)
+}) {
+
+    lateinit var service: SoptService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,35 +28,33 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>({
         signUpNetwork()
     }
 
-    private fun initEvent(){
-        binding.btnSignupend.setOnClickListener{
+    private fun initEvent() {
+        binding.btnSignupend.setOnClickListener {
             signUpNetwork()
         }
     }
 
-    private fun signUpNetwork(){
+    private fun signUpNetwork() {
         val requestSignUp = RequestSignUp(
             name = binding.nameEdit.text.toString(),
             id = binding.idEdit.text.toString(),
             password = binding.pwEdit.text.toString()
         )
 
-        val call: Call<ResponseSignUp> = ServiceCreator.soptService.postSignUp(requestSignUp)
-
-        call.enqueueUtil(
-            onSuccess = {
+        lifecycleScope.launch {
+            runCatching {
+                service.postSignUp(requestSignUp)
+            }.onSuccess {
                 val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
                 intent.putExtra("edit_id", binding.idEdit.text.toString())
                 intent.putExtra("edit_pw", binding.pwEdit.text.toString())
                 setResult(RESULT_OK, intent)
-                finish()
-            },
-            onError = {
-                when(it){
-                    409 -> showToast("이미 존재하는 유저입니다.")
-                    else -> showToast("회원가입에 실패하였습니다.")
+                if (!isFinishing) {
+                    finish()
                 }
+            }.onFailure {
+                showToast("$it")
             }
-        )
+        }
     }
 }
