79ce8f906a24f3127c212d62193c4047fd503ffb
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-08-03 23:04:06 +0900

    [fix/#7] service 오류 해결

--- app/src/main/java/com/example/soptseminar1/presentation/profile/repository/RepositoryFragment.kt
+++ app/src/main/java/com/example/soptseminar1/presentation/profile/repository/RepositoryFragment.kt
@@ -2,13 +2,16 @@ package com.example.soptseminar1.presentation.profile.repository
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.example.soptseminar1.data.api.GithubApiCreator
+import com.example.soptseminar1.data.api.GithubApiService
 import com.example.soptseminar1.data.model.response.ResponseGithubUserRepo
 import com.example.soptseminar1.databinding.FragmentRepositoryBinding
 import com.example.soptseminar1.util.enqueueUtil
 import com.example.soptseminar1.util.MyItemDecoration
 import com.example.soptseminar1.util.BaseFragment
 import com.example.soptseminar1.util.showToast
+import kotlinx.coroutines.launch
 import retrofit2.Call
 
 class RepositoryFragment : BaseFragment<FragmentRepositoryBinding>(FragmentRepositoryBinding::inflate) {
@@ -27,18 +30,16 @@ class RepositoryFragment : BaseFragment<FragmentRepositoryBinding>(FragmentRepos
     }
 
     private fun repoNetwork() {
-        val call: Call<List<ResponseGithubUserRepo>> =
-            GithubApiCreator.githubApiService.fetchGithubRepos()
-
-        call.enqueueUtil(
-            onSuccess = {
+        lifecycleScope.launch {
+            runCatching {
+                GithubApiCreator.githubApiService.fetchGithubRepos()
+            }.onSuccess {
                 it.let {
                     repositoryAdapter.submitList(it)
                 }
-            },
-            onError = {
-                requireContext().showToast("불러오기에 실패했습니다.")
+            }.onFailure {
+                requireContext().showToast("$it")
             }
-        )
+        }
     }
 }
