da01db4fbc0adc3027e029e78b5cf5d611d9c394
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-08-08 23:33:29 +0900

    [refactor/#7] 모듈 추가

---
+++ app/src/main/java/com/example/soptseminar1/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.example.soptseminar1.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient,
+        gson: Gson
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().setLenient().create()
+
+    const val BASE_URL = "https://api.github.com"
+}
